046a71c5e9007e3d7e76b00e33c713baad6111fb
Abdallah <abdallahkhafagy73@gmail.com>
2026-07-22 16:30:20 +0300

    Add logger & ApiResponse Class

--- src/app.controller.js
+++ src/app.controller.js
@@ -7,10 +7,13 @@ import { ZodError } from "zod";
 import multer from "multer";
 import { BadRequestError } from "./utils/error/index.js";
 import routes from "./routes/index.js";
+import logger from "./utils/logger.js";
+import pinoHttp from "pino-http";
 
 export async function bootStrap(app, express) {
   app.use(express.json());
   app.use(cors({ origin: "*" }));
+  app.use(pinoHttp({ logger }));
 
   app.use("/api", routes);
 
