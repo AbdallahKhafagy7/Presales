ecc1a0c07b1d394369ead1e8f2cbd5e4cca61124
ziad <ziadmogadam@gmail.com>
2026-07-22 15:01:42 +0300

    Global Error-Handling Middleware (task 5)

--- src/app.controller.js
+++ src/app.controller.js
@@ -3,9 +3,10 @@ import cors from "cors"
 // import requirementController from "./module/Requirements/requirement.controller.js"
 // import requirementFileController from "./module/RequirementFile/requirementFile.controller.js"
 // import opportunityAnalysisController from "./module/opportunity-analysis/opportunityAnalysis.controller.js"
-import { ZodError } from "zod";
-import multer from "multer";
-import { BadRequestError } from "./utils/error/index.js"
+
+import { globalErrorHandler } from "./utils/middleware/globalErrorHandler.js";
+import { ApiError } from "./utils/error/errorClass.js";
+
 
 
 export async function bootStrap(app, express) {
@@ -16,33 +17,13 @@ export async function bootStrap(app, express) {
     // app.use("/requirement", requirementController);
     // app.use("/requirement-file", requirementFileController);
 
+    
+    app.get("/",(req,res)=>{
+        throw new ApiError(404,"ziad");
+    })
     //global error handling
-    app.use((err, req, res, next) => {
-        if (
-            err instanceof multer.MulterError
-        ) {
-            switch (err.code) {
-                case "LIMIT_FILE_SIZE":
-                    return res.status(400).json({
-                        message:
-                            "File size must not exceed 5 MB",
-                    });
-            }
-        }
-        if (err instanceof ZodError) {
-            return res.status(400).json({
-                message: "Validation failed",
-                errors: err.issues.map(issue => ({
-                    field: issue.path.join("."),
-                    message: issue.message,
-                })),
-            });
-        }
-        res.status(err.statusCode || 500).json({
-            message: err.message || "Something went wrong",
-        });
-    }
-    );
+    app.use(globalErrorHandler);
+
     //for any dummy requestes (not handled)
     app.use("/:dummy", (req, res, next) => {
         res.status(404).json({ message: "invalid url" })
