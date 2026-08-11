44fa97d4fa9fd3860db71c51e4935eea5e1075b3
ziad <ziadmogadam@gmail.com>
2026-08-11 13:10:55 +0300

    apis update

--- src/routes/opportunityEstimation.js
+++ src/routes/opportunityEstimation.js
@@ -3,15 +3,15 @@ import * as estimationController from "../module/opportunity-estimation/opportun
 import { validate } from "../utils/middleware/zod.validation.js";
 import { objectIdvalidateSchema } from "../module/comman/validation.js";
 const router = new Router();
-router.get("/:id/estimation/opportunity",
+router.get("/:id/estimations/opportunity",
     validate(objectIdvalidateSchema),
     estimationController.getOpportunity
 );
-router.get("/:id/estimation/context",
+router.get("/:id/estimations/context",
     validate(objectIdvalidateSchema),
     estimationController.getContext
 )
-router.post("/:id/estimation/generate",
+router.post("/:id/estimations/generate",
     validate(objectIdvalidateSchema),
     estimationController.generateEstimation)
 export default router;
